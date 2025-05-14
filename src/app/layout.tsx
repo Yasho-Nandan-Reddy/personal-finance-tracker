0b1fb3204f4811ec222b8c1d3d8cecdec3df549f
Yasho-Nandan-Reddy <sangireddy.ynr@gmail.com>
2025-05-14 12:00:53 +0530

    Second commit: Personal Finance Tracker application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/route";
 import SessionProvider from "@/components/SessionProvider";
 import Navigation from "@/components/Navigation";
 
